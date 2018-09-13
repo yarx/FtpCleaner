b8f46626028240cf4d515e265a6c297588338a35
Raphael Bolliger <raphibolliger@hotmail.com>
2018-09-13 20:32:54 +0200

    Add project files.

---
+++ FtpClean/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FtpClean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
